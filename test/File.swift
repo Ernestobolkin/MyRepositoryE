dfd964a536ff1db396cc7d8290c2d61702354cb7
Ernestobolkin <92053947+Ernestobolkin@users.noreply.github.com>
2021-10-31 08:39:55 +0200

    Add files via upload

---
+++ test/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  test
+//
+//  Created by Ernest Tobolkin on 24/09/2020.
+//
+
+import Foundation
+// Hello, World! Program
+import Swift
+print("Hello, World!")
+
---
+++ test/test/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  test
+//
+//  Created by Ernest Tobolkin on 24/09/2020.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
